b06852ad9678df142e55697e795c27853618ed1d
miguel79pv <miguel79pv@gmail.com>
2021-01-28 23:10:41 -0600

    entregable de mercado V1

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require ('express');
+const path = require('path');
+const app = express();
+const publicPath = path.resolve(__dirname, './public');
+app.use('/static', express.static(__dirname + '/public'));
+
+app.get('/registro', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'views/registro.html'));
+});
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'views/home.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+
